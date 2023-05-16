884840561713a1a12a0d1bb1284cb068547748f6
skripchak <skripchakandreywork@gmail.com>
2023-05-16 22:38:09 +0300

    Created parser for url in autoria site.

---
+++ puppeteer.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+// const numbers = [];
+
+async function parseAutoRia(url = "https://auto.ria.com/uk/auto_volvo_xc90_34163215.html") {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    // const url = 'https://auto.ria.com/uk/auto_volvo_xc90_34163215.html';
+    await page.goto(url);
+
+    const phoneElement = await page.$('.phone');
+    if (phoneElement) {
+        const number = await page.$eval('.phone', element => element.textContent.trim());
+        await phoneElement.click();
+        await page.waitForTimeout(3000);
+        const phoneNumber = await page.$eval('.phone', element => element.textContent.trim());
+        console.log(phoneNumber);
+    }
+
+    await browser.close();
+}
+
+parseAutoRia()
