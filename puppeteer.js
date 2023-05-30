5b51fd9c9eaf7beab834f0684deeb05aeb1eaed7
skripchak <skripchakandreywork@gmail.com>
2023-05-30 21:28:58 +0300

    Speed up the parser.

--- puppeteer.js
+++ puppeteer.js
@@ -1,59 +1,28 @@
 const puppeteer = require('puppeteer');
 
-// const numbers = [];
-
-async function parseAutoRia(url = "/auto_volvo_xc90_34163215.html") {
+async function parseAutoRia(urls) {
     const browser = await puppeteer.launch({timeout: 0});
     const page = await browser.newPage();
+    const phones = [];
 
-    await page.goto(url);
-
-    const phoneElement = await page.$('.phone');
-    if (phoneElement) {
-        const number = await page.$eval('.phone', element => element.textContent.trim());
-        await phoneElement.click();
-        await page.waitForTimeout(3000);
-        const phoneNumber = await page.$eval('.phone', element => element.textContent.trim());
-        if (phoneNumber !== undefined && phoneNumber !== null) {
-            console.log(phoneNumber);
-            await browser.close();
-            return phoneNumber;
+    for (let url of urls) {
+        console.log(url);
+        await page.goto(url, {waitUntil: 'networkidle0'});
+        const phoneElement = await page.$('.phone');
+        if (phoneElement) {
+            const number = await page.$eval('.phone', element => element.textContent.trim());
+            await phoneElement.click();
+            await page.waitForTimeout(4000);
+            const phoneNumber = await page.$eval('.phone', element => element.textContent.trim());
+            if (phoneNumber !== undefined && phoneNumber !== null) {
+                console.log(phoneNumber);
+                phones.push(phoneNumber);
+            }
         }
     }
+    return phones;
 }
 
-function getJson(data) {
-    const autoria_url = 'https://auto.ria.com/uk'
-    let phones = [];
-    const promises = data.pages.map((e) => {
-        return parseAutoRia(autoria_url + e);
-    });
-
-    Promise.all(promises)
-        .then((phoneNumbers) => {
-
-            const url = 'http://127.0.0.1:8000/get_phones/';
-
-            const requestOptions = {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(phoneNumbers),
-            };
-
-            console.log(JSON.stringify(phoneNumbers));
-
-            fetch(url, requestOptions)
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data);
-                })
-                .catch(error => {
-                    console.error('Ошибка при выполнении POST-запроса:', error);
-                });
-        });
-}
 
 async function parseSearch(data, url = "https://auto.ria.com/uk/advanced-search/") {
     const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
@@ -69,7 +38,6 @@ async function parseSearch(data, url = "https://auto.ria.com/uk/advanced-search/
 
     if (data.select.length !== 0) {
         for (let select of data.select) {
-            console.log(select);
             const selectElement = await page.$(select.element);
             await selectElement.focus();
             await page.keyboard.press('ArrowDown');
@@ -115,53 +83,37 @@ async function parseSearch(data, url = "https://auto.ria.com/uk/advanced-search/
     await page.evaluate((e) => e.click(), submit);
     await page.waitForTimeout(10000);
     await page.screenshot({path: 'screenshot2.png', fullPage: true});
-    console.log('212');
 
 
     if (await page.$('.item.ticket-title a')) {
         const hrefs = await page.$$eval('.item.ticket-title a', links => links.map(link => link.href));
         await browser.close();
-        let phones = [];
-        const promises = hrefs.map((e, index) => {
-            return new Promise(resolve => {
-                setTimeout(async () => {
-                    const result = await parseAutoRia(e);
-                    resolve(result);
-                }, index * 10000);
-            });
-        });
-        Promise.all(promises)
-            .then((phoneNumbers) => {
 
-                const url = 'http://127.0.0.1:8000/get_phones/';
+        const phoneNumbers = await parseAutoRia(hrefs);
+        if (phoneNumbers.length > 0) {
+            const url = 'http://127.0.0.1:8000/get_phones/';
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(phoneNumbers),
+            };
 
-                const requestOptions = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify(phoneNumbers),
-                };
+            console.log(JSON.stringify(phoneNumbers));
 
-                console.log(JSON.stringify(phoneNumbers));
-
-                fetch(url, requestOptions)
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log(data);
-                    })
-                    .catch(error => {
-                        console.error('Ошибка при выполнении POST-запроса:', error);
-                    });
-            });
+            fetch(url, requestOptions)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.error('Ошибка при выполнении POST-запроса:', error);
+                });
+        }
     }
-
-
-    await page.waitForTimeout(2000);
-
 }
 
 module.exports = {
-    getJson,
     parseSearch
 };
--- server.js
+++ server.js
@@ -2,25 +2,6 @@ const http = require('http');
 const {getJson, parseSearch} = require('./puppeteer.js');
 
 const server = http.createServer((req, res) => {
-    if (req.method === 'POST' && req.url === '/data') {
-        let data = '';
-
-        req.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        req.on('end', () => {
-            const jsonData = JSON.parse(data);
-
-            getJson(jsonData)
-
-            res.statusCode = 200;
-            res.end('OK');
-        });
-    } else {
-        res.statusCode = 404;
-        res.end('Not Found');
-    }
     if (req.method === 'POST' && req.url === '/') {
         let data = '';
 
