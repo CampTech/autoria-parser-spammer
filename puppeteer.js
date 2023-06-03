bfb6ea4dabbfe4230f30f4f80188e0c2b4dedba4
skripchak <skripchakandreywork@gmail.com>
2023-06-04 00:53:43 +0300

    Fixed arguments to launcher chrome.

--- puppeteer.js
+++ puppeteer.js
@@ -1,7 +1,7 @@
 const puppeteer = require('puppeteer');
 
 async function parseAutoRia(urls) {
-    const browser = await puppeteer.launch({timeout: 0});
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], timeout: 0});
     const page = await browser.newPage();
     const phones = [];
 
